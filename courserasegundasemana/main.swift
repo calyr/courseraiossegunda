8e650a9be57a9c568a6c471c5286b243f30bae98
calyr <calyr_software@hotmail.com>
2016-05-02 23:55:46 -0400

    Initial Commit

---
+++ courserasegundasemana/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  courserasegundasemana
+//
+//  Created by calyr on 5/2/16.
+//  Copyright © 2016 calyr. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
